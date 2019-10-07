d2cac60cfbc019176e22d9930d47c1f2bcd9cc4a
Iain Duff <iainrobertduff@gmail.com>
2019-10-07 18:58:51 +0400

    4th commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Auto Test Site`,
+    title: `Hello Sophie`,
     description: `An awesome blog displaying my awesome posts.`,
     author: `Iain Duff`,
 },
