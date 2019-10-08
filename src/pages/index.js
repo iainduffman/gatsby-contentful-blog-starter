2fa12f6aec32eecea35a4b9bdb06667657deb3f8
Iain Duff <iainrobertduff@gmail.com>
2019-10-09 01:07:58 +0400

    Design updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,10 @@ const BlogPosts = ({ data }) => {
             <div className="uk-container heroContent uk-text-center">
             <h3>Want Amazing Deals + Special Offers?</h3>
             <h1>You’ve Come To The Right Place</h1>
+            <div className="buttonWrapper">
             <button className="uk-button uk-button-primary uk-button-large">Large button</button>
+            <button className="uk-button uk-button-secondary uk-button-large">Large button</button>
+            </div>
             </div>
             </div>
         </div>
