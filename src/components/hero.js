b409aa0ebf92853e758b4014db5affcfe85a5c10
Iain Duff <iainrobertduff@gmail.com>
2019-10-10 10:57:04 +0400

    Homepage change

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,7 +2,6 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import React from "react"
-import "./footer.css"
 
 export default props => (
 
